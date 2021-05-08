205194b2e8e08007485c5c6e09fc2ce339a30d3b
haukun <alt-github@projecgroom.jp>
2021-05-08 09:52:11 +0900

    『一つ　焔はその身を焦がし』
    南の方位にサラマンダーに呼びかける陣を描きだす。

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function draw() {
   stroke(255);
   fill(0);
   for (var i = 1; i < 3; i++) {
-    var offset = i * PI / 4;
+    var offset = (i * PI) / 4;
     beginShape();
     for (var angle = 0; angle < TAU; angle += PI / 2) {
       var adjustAngle = angle + offset;
@@ -57,13 +57,13 @@ function draw() {
     rotate(angle);
 
     for (i = 0; i < 3; i++) {
-      var randomAngle = int(random(6)) * PI / 3;
+      var randomAngle = (int(random(6)) * PI) / 3;
       var endX = cos(randomAngle) * 10;
       var endY = sin(randomAngle) * 5;
-      var bezierAngle1 = int(random(6)) * PI / 3;
+      var bezierAngle1 = (int(random(6)) * PI) / 3;
       var controlX1 = cos(bezierAngle1) * 10;
       var controlY1 = sin(bezierAngle1) * 5;
-      var bezierAngle2 = int(random(6)) * PI / 3;
+      var bezierAngle2 = (int(random(6)) * PI) / 3;
       var controlX2 = cos(bezierAngle2) * 10;
       var controlY2 = sin(bezierAngle2) * 5;
       bezier(0, 0, controlX1, controlY1, controlX2, controlY2, endX, endY);
@@ -78,13 +78,36 @@ function draw() {
   stroke(255);
   fill(0);
   for (var angle = 0; angle < TAU; angle += PI / 2) {
-    strokeWeight(1)
+    strokeWeight(1);
     circle(cos(angle) * 310 + 360, sin(angle) * 310 + 360, 85);
-    strokeWeight(3)
+    strokeWeight(3);
     circle(cos(angle) * 310 + 360, sin(angle) * 310 + 360, 75);
-    strokeWeight(1)
+    strokeWeight(1);
     circle(cos(angle) * 310 + 360, sin(angle) * 310 + 360, 65);
   }
   pop();
 
+  //  Fire circle
+  push();
+  fill(0);
+  noStroke();
+  var baseX = cos((TAU * 1) / 4) * 310 + 360;
+  var baseY = sin((TAU * 1) / 4) * 310 + 360;
+
+  circle(baseX, baseY, 65);
+  drawingContext.clip();
+
+  noFill();
+  stroke(255);
+  for (angle = 0; angle < TAU; angle += PI / 4) {
+    arc(
+      baseX + cos(angle) * 50,
+      baseY + sin(angle) * 50,
+      40,
+      40,
+      angle - PI / 4 + PI,
+      angle + PI / 4 + PI
+    );
+  }
+  pop();
 }
